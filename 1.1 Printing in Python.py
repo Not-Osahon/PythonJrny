f324aa1532d952a5b19e47e5e481d58edb91a26a
Not-Osahon <akbosa04@gmail.com>
2024-12-10 17:38:19 +0100

    2nd Commit

---
+++ 1.1 Printing in Python.py
@@ -0,0 +1,36 @@
+print("Hello, World!")
+print("Today is 9th of December 2024.")
+print("I am going to master this language and do great things with it")
+print('Lets begin this whole journey')
+print("Let's all be guided please!")
+
+print("Now unto more advanced things!")
+print("This is first\nThis will be second")
+print("S/N\tName\tCourse\tSchool")
+
+#printing multiple things with commas
+print("My name is","Osahon","and i am 20 years old." )
+
+#Formatting Strings
+name = "Alice"
+age = 40
+print(f"{name} is {age} years old\\or not.")
+#Advanced print options
+print(f"{name} is a very intelligent woman.\nShe is only {age} years old", end=".\n")
+print("Hello", "World", "It's me Again",sep="-")
+print("Hello","World")
+print("Hello" + " ""World\n")
+print("Hello""World")
+
+#age = 30
+#print("I am " + str(age) + " years old.")  # Correct
+#print("I am", age ,"years old.")  # TypeError
+#print("hi")
+
+#MULTI LINE PRINTING
+print("""This is line 1.
+This is line 2.
+This is line 3.""")
+print("This is line 4.\nThis is line 5.")
+print('''Line 6.
+and so on and so forth....''')
