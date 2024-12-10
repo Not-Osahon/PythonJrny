165944d097f54475ed00446ab44bbfca6b5d6163
Not-Osahon <akbosa04@gmail.com>
2024-12-10 19:25:56 +0100

    Data Types

---
+++ Data types in Python.py
@@ -0,0 +1,12 @@
+# Understanding Data types in Python
+# Common Data Types
+# Numbers
+1, 2, 3 # Integers
+2.1 , -2.4 # Float
+True, False # Boolean(bool)
+"This is a string" # string(str)
+
+# Check data type
+# type function
+age = 10
+print(type(age))
