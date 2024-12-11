b668e145ef517805453d85bcaf381960c0fb5700
Not-Osahon <akbosa04@gmail.com>
2024-12-11 01:21:54 +0100

    Data Types

--- Data types in Python.py
+++ Data types in Python.py
@@ -10,3 +10,17 @@ True, False # Boolean(bool)
 # type function
 age = 10
 print(type(age))
+
+is_rainy = True
+result_not = not is_rainy
+print(result_not)
+
+float_var = 2.60
+formatted_strings = f"John's height is equal to {float_var:.2f}"
+formatted_strings1 = f"John's height is equal to ", {float_var}
+print(f"John's height is equal to ", {float_var})
+print(f"John's height is equal to {float_var:.2f}")
+print(formatted_strings)
+print(formatted_strings1)
+
+
